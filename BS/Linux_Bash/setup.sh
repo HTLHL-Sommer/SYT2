d36389447a36f29d9cfa3879b2ccc0da6e08b0e3
Paul Sommer <paul@sommers.dev>
2026-03-16 13:08:50 +0100

    16.3.2026

--- BS/Linux_Bash/setup.sh
+++ BS/Linux_Bash/setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+TARGET_DIR="./linux_uebung"
+
+# 1. Überprüfen, ob der Ordner existiert
+if [ -d "$TARGET_DIR" ]; then
+  echo "Ordner existiert. Lösche $TARGET_DIR"
+  # 2. Ordner mit Inhalt rekrusiv (-r) und
+  # erzwungen (-f) löschen
+  rm -rf "$TARGET_DIR"
+else
+  echo "Ordner $TARGET_DIR existiert NICHT!"
+fi
+
+echo "Ordner $TARGET_DIR wird aufgebaut..."
+
 mkdir -p linux_uebung/dokumente
 mkdir -p linux_uebung/bilder
 mkdir -p linux_uebung/downloads
